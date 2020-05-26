b4663bcc87bae14b8672a7f9c5851b739ac8ee41
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-05-26 20:32:11 -0300

    variables + output function + version control options

--- run.sh
+++ run.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "Please 'sudo ./run.sh'" 
-   exit 1
+    echo "Please 'sudo ./run.sh'"
+    exit 1
 fi
 
-echo "Note that still only works with Ubuntu"
+function output() {
+    array=($(echo $1 | tr "|" "\n"))
 
-zenity --list --checklist --title="Packages"\
-    --text="Select what you want to install "\
-    --column=""\
-    --column="Package name"\
-    false A\
-    false B\
+    for i in "${array[@]}"; do
+        $i
+    done
+}
+
+zenity \
+    --info \
+    --no-wrap \
+    --text="Note that still only works with Ubuntu"
+
+function git() {
+    echo "git installation"
+}
+
+function git-flow() {
+    echo "git-flow installation"
+}
+
+input=$(zenity \
+    --list \
+    --checklist \
+    --title="Packages" \
+    --text="Version control" \
+    --column="Check" \
+    --column="Package name" \
+    false git \
+    false git-flow)
+
+output $input
