cc1c6acef15cd58b0c409be18b49121ba8b9b28c
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-05-26 13:28:26 -0300

    list

--- run.sh
+++ run.sh
@@ -4,3 +4,10 @@ if [[ $EUID -ne 0 ]]; then
    echo "Please 'sudo ./run.sh'" 
    exit 1
 fi
+
+zenity --list --checklist --title="Packages"\
+    --text="Select what you want to install "\
+    --column=""\
+    --column="Package name"\
+    false A\
+    false B\
