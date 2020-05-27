d95d6aae3fdd09fa4a27a7718e65493af6404f2a
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-05-27 19:08:21 -0300

    [WIP] looping on apt install

--- run.sh
+++ run.sh
@@ -4,6 +4,18 @@ if [[ $EUID -ne 0 ]]; then
     echo "Please 'sudo ./run.sh'"
     exit 1
 fi
+globalInput=""
+
+function getInput() {
+    globalInput=$(zenity \
+        --entry \
+        --title="$1" \
+        --text="$2")
+}
+
+function install() {
+    sudo apt-get --yes install $@
+}
 
 function output() {
     array=($(echo $1 | tr "|" "\n"))
@@ -13,18 +25,30 @@ function output() {
     done
 }
 
-zenity \
-    --info \
-    --no-wrap \
-    --text="Note that still only works with Ubuntu"
-
 function git() {
-    echo "git installation"
+    install git
+
+    git config --global core.fileMode false
+    git config --global credential.helper store
+
+    getInput "GIT" "Default commit NAME"
+    git config --global user.name "$globalInput"
+    getInput "GIT" "Default commit EMAIL"
+    git config --global user.email "$globalInput"
 }
 
 function git-flow() {
     echo "git-flow installation"
 }
+
+#------#
+# MAIN #
+#------#
+
+zenity \
+    --info \
+    --no-wrap \
+    --text="Note that still only works with Ubuntu"
 
 input=$(zenity \
     --list \
