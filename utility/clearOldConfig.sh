f59adac71fe3fdf203c6b7881c7bf1320d08db63
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-11-17 11:18:57 -0300

    :sparkles: now removes old snaps

--- utility/clearOldConfig.sh
+++ utility/clearOldConfig.sh
@@ -3,3 +3,12 @@
 # If it says that needs at least one package, it means that you don't need it now
 
 sudo dpkg -l | grep "^rc" | awk '{print $2}' | sudo xargs dpkg -P
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
