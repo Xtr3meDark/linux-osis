a0e43873574248e43440a492b5c20265064b483f
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-11-23 09:53:01 -0300

    :bug: fix ventoy wrong download folder

--- run.sh
+++ run.sh
@@ -51,7 +51,7 @@ function output() {
 # BOOT
 
 function install_ventoy() {
-    cd $DOWNLOADS
+    cd $HOME/Downloads
     curl -s https://api.github.com/repos/ventoy/Ventoy/releases/latest \
     | grep "browser_download_url" \
     | grep "linux" \
