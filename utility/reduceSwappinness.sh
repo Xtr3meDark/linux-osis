f2d74e7f59e8aa3ab81b76d2c5d746cb02ddb1bf
Leonardo Renaut <xxforagidoxx@gmail.com>
2020-11-30 18:42:48 -0300

    :sparkles: swap and cache fix

--- utility/reduceSwappinness.sh
+++ utility/reduceSwappinness.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-sudo sysctl vm.swappiness=10
 clear
-echo Swappiness reduced to: 
-cat /proc/sys/vm/swappiness
+
+sudo sysctl vm.swappiness=5
+sudo sysctl vm.vfs_cache_pressure=50
+
+echo "" | sudo tee /etc/sysctl.d/99-sysctl.conf -a
+echo "vm.swappiness=5" | sudo tee /etc/sysctl.d/99-sysctl.conf -a
+echo "vm.vfs_cache_pressure=50" | sudo tee /etc/sysctl.d/99-sysctl.conf -a
+
+clear
+echo Swappiness and cache reduced to 5 and 50
